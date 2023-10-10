04b2533019577dd64fe9dd32b11510f5cefa6fa6
Sandro <sandrovprado@gmail.com>
2023-10-10 14:12:30 -0400

    Initial commit

---
+++ src/main/java/com/dorm/Main.java
@@ -0,0 +1,11 @@
+package com.dorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
